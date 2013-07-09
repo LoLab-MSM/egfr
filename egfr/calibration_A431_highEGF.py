c6d1d73e7255c5d7a2c50dfd7dc1fc49116ea3dd
Erin Shockley <Erin.Shockley@Vanderbilt.edu>
2013-07-09 17:38:51 -0500

    Calibration file for unnormalized splined data for high EGF condition

--- egfr/calibration_A431_highEGF.py
+++ egfr/calibration_A431_highEGF.py
@@ -6,9 +6,16 @@ import numpy
 import matplotlib.pyplot as plt
 import os
 import itertools
+import pickle
 
 from erbb_exec import model
 
+with open('calibration_A431_highEGF_fittedparams_norm_3.txt', 'rb') as handle:
+    fittedparams = pickle.loads(handle.read())
+
+for i in range(len(model.parameters)):
+    if model.parameters[i].name in fittedparams:
+        model.parameters[i].value = fittedparams[model.parameters[i].name]
 
 def normalize(trajectories):
     """Rescale a matrix of model trajectories to 0-1"""
@@ -24,9 +31,9 @@ def likelihood(mcmc, position):
     """Distance between model trajectories and experimental data"""
     ysim = mcmc.simulate(position, observables=True)
     ysim_array = extract_records(ysim, obs_names)
-    ysim_norm = normalize(ysim_array)
     #FIXME: exp_var is really a sdev (and so is prior_var)
-    return numpy.sum((ydata_norm - ysim_norm) ** 2 / (2 * exp_var ** 2))
+    # Column 0 = AKTPP, column 1 = ErbB1P, column 2 = ERKPP
+    return numpy.sum((ydata_exp - ysim_array) ** 2 / (2 * exp_var ** 2))
 
 def prior(mcmc, position):
     """Distance to original parameter values"""
@@ -40,13 +47,12 @@ def step(mcmc):
              mcmc.accept_likelihood, mcmc.accept_prior, mcmc.accept_posterior)
 
 # data is already scaled to 0-1
-data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_A431_highEGF.npy')
-ydata_norm = numpy.load(data_filename)
-var_data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_var_A431_highEGF.npy')
-exp_var = numpy.load(var_data_filename) #Standard deviation was calculated from the mean by assuming a coefficient of variation of .25; sdev's equal to 0 were set to 1 to avoid division by 0 errors.
+data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_A431_highEGF_unnorm_splined.npy')
+ydata_exp = numpy.load(data_filename)
+var_data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_var_A431_highEGF_unnorm_splined.npy')
+exp_var = numpy.load(var_data_filename) #Standard deviation was calculated from the mean by assuming a coefficient of variation of .25; sdev's equal to 0 were set to 1 to avoid division by 0 errors
 
-# Convergance criteria: posterior should be less than 8.36 (the sum of all experimental variances).
-tspan = numpy.array([0., 150., 300., 450., 600., 900., 1800., 2700., 3600., 7200.]) #10 unevenly spaced time points
+tspan = numpy.linspace(0,7200, num=1000)
 
 obs_names = ['obsAKTPP', 'obsErbB1_ErbB_P_CE', 'obsERKPP']
 
@@ -54,7 +60,7 @@ opts = bayessb.MCMCOpts()
 opts.model = model
 opts.tspan = tspan
 opts.integrator = 'vode'
-opts.nsteps = 50000
+opts.nsteps = 25000
 
 scenario = 1
 
@@ -75,7 +81,7 @@ else:
 
 # values for prior calculation
 prior_mean = [numpy.log10(p.value) for p in opts.estimate_params]
-# prior_var is set to 6.0 so that (since calc is in log space) parameters can vary within 6 orders of magnitude and not be penalized.
+# prior_var is set to 3.0 so that (since calc is in log space) parameters can vary within 3 orders of magnitude and not be penalized.
 prior_var =  6.0
 
 
@@ -95,32 +101,18 @@ mcmc.run()
 print
 print '%-10s %-12s %-12s %s' % ('parameter', 'actual', 'fitted', 'log10(fit/actual)')
 fitted_values = mcmc.cur_params()[mcmc.estimate_idx]
+param_dict = {}
 for param, new_value in zip(opts.estimate_params, fitted_values):
     change = numpy.log10(new_value / param.value)
     values = (param.name, param.value, new_value, change)
     print '%-10s %-12.2g %-12.2g %-+6.2f' % values
+    param_dict[param.name] = new_value
 
-# plot data and simulated trajectories before and after the fit
-colors = ('r', 'g', 'b')
-patterns = ('k--', 'k', 'k:x')
-# generate a legend with the deconvolved colors and styles
-for style in colors + patterns:
-    plt.plot(numpy.nan, numpy.nan, style)
-plt.legend(('pAKT', 'pErbB1', 'pERK', 'initial', 'final', 'data'), loc='lower right')
-# simulate initial and final trajectories and plot those along with the data
-yinitial = pysb.integrate.odesolve(model, tspan)
-yinitial_array = extract_records(yinitial, obs_names)
-yfinal_array = extract_records(mcmc.simulate(observables=True), obs_names)
-initial_lines = plt.plot(tspan, normalize(yinitial_array))
-data_lines = plt.plot(tspan, ydata_norm)
-final_lines = plt.plot(tspan, normalize(yfinal_array))
-for il, dl, fl, c in zip(initial_lines, data_lines, final_lines, colors):
-    il.set_color(c)
-    dl.set_color(c)
-    fl.set_color(c)
-    il.set_linestyle('--')
-    dl.set_linestyle(':')
-    dl.set_marker('x')
-plt.show()
-numpy.save('calibration_allpositions_A431_highEGF.npy', mcmc.get_mixed_accepts(burn=opts.nsteps/10))
-numpy.save('calibration_fittedparams_A431_highEGF.npy', zip(opts.estimate_params, mcmc.cur_params()[mcmc.estimate_idx]))
+with open('calibration_A431_highEGF_fittedparams_unnorm_splined_1.txt', 'wb') as handle:
+    pickle.dump(param_dict, handle)
+
+numpy.save('calibration_allpositions_A431_highEGF_unnorm_splined_1.npy', mcmc.get_mixed_accepts(burn=opts.nsteps/10))
+numpy.save('calibration_fittedparams_A431_highEGF_unnorm_splined_1.npy', zip(opts.estimate_params, mcmc.cur_params()[mcmc.estimate_idx]))
+
+
+
