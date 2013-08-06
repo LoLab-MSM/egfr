40867dd1f830351310358e0b4b22152461c17020
Erin Shockley <Erin.Shockley@Vanderbilt.edu>
2013-08-06 16:54:34 -0500

    Changed calibration script

--- egfr/calibration_H1666_findingnans.py
+++ egfr/calibration_H1666_findingnans.py
@@ -40,38 +40,38 @@ def likelihood(mcmc, position):
         objAKT[mcmc.iter] = numpy.sum((yslice_exp_he[:,0] - ysim_slice_he[:,0]) ** 2 / (2 * yslice_exp_var_he[:,0] ** 2))
         objErb[mcmc.iter] = numpy.sum((yslice_exp_he[:,1] - ysim_slice_he[:,1]) ** 2 / (2 * yslice_exp_var_he[:,1] ** 2))
         objERK[mcmc.iter] = numpy.sum((yslice_exp_he[:,2] - ysim_slice_he[:,2]) ** 2 / (2 * yslice_exp_var_he[:,2] ** 2))
-        mcmc.positions[mcmc.iter,:] = mcmc.test_position
-        if numpy.isnan(objAKT[mcmc.iter]) or numpy.isnan(objErb[mcmc.iter]) or numpy.isnan(objERK[mcmc.iter]) is True:
-            #print some information about the maximum-likelihood estimate parameter set
-            print
-            print '%-10s %-12s %-12s %s' % ('parameter', 'actual', 'fitted', 'log10(fit/actual)')
-            fitted_values = mcmc.cur_params()[mcmc.estimate_idx]
-            param_dict = {}
-            for param, new_value in zip(opts.estimate_params, fitted_values):
-                change = numpy.log10(new_value / param.value)
-                values = (param.name, param.value, new_value, change)
-                print '%-10s %-12.2g %-12.2g %-+6.2f' % values
-                param_dict[param.name] = new_value
+        # mcmc.positions[mcmc.iter,:] = mcmc.test_position
+        # if numpy.isnan(objAKT[mcmc.iter]) or numpy.isnan(objErb[mcmc.iter]) or numpy.isnan(objERK[mcmc.iter]) is True:
+        #     #print some information about the maximum-likelihood estimate parameter set
+        #     print
+        #     print '%-10s %-12s %-12s %s' % ('parameter', 'actual', 'fitted', 'log10(fit/actual)')
+        #     fitted_values = mcmc.cur_params()[mcmc.estimate_idx]
+        #     param_dict = {}
+        #     for param, new_value in zip(opts.estimate_params, fitted_values):
+        #         change = numpy.log10(new_value / param.value)
+        #         values = (param.name, param.value, new_value, change)
+        #         print '%-10s %-12.2g %-12.2g %-+6.2f' % values
+        #         param_dict[param.name] = new_value
 
-                with open('calibration_H1666_highEGF_findingnans2', 'wb') as handle:
-                    pickle.dump(param_dict, handle)
+        #         with open('calibration_H1666_highEGF_findingnans7', 'wb') as handle:
+        #             pickle.dump(param_dict, handle)
 
-                    name = [p.name for p in opts.estimate_params]
-                    oldvalues = [p.value for p in opts.estimate_params]
-                    newvalues = mcmc.cur_params()[mcmc.estimate_idx]
-                    oldvalues_array = numpy.array(oldvalues)
-                    newvalues_array = numpy.array(newvalues)
-                    change = numpy.log10(newvalues_array / oldvalues_array)
-                    combined = numpy.column_stack((name, oldvalues, newvalues, change))
-                    numpy.savetxt('calibration_H1666_highEGF_paramchange_findingnans2.txt', combined, delimiter=' ', fmt='%s')
-                    numpy.save('calibration_alltestedparams_H1666_highEGF_findingnans2.npy', mcmc.positions)
-                    numpy.save('calibration_allpositions_H1666_highEGF_findingnans2.npy', mcmc.get_mixed_accepts(burn=opts.nsteps/10))
-                    numpy.save('calibration_fittedparams_H1666_highEGF_findingnans2.npy', zip(opts.estimate_params, mcmc.cur_params()[mcmc.estimate_idx]))
-                    numpy.save('calibration_likelihoods_H1666_highEGF_findingnans2.npy', mcmc.likelihoods)
-                    numpy.save('calibration_AKTlikelihoods_H1666_highEGF_findingnans2.npy', objAKT)
-                    numpy.save('calibration_Erblikelihoods_H1666_highEGF_findingnans2.npy', objErb)
-                    numpy.save('calibration_ERKlikelihoods_H1666_highEGF_findingnans2.npy', objERK)
-                    sys.exit()
+        #             name = [p.name for p in opts.estimate_params]
+        #             oldvalues = [p.value for p in opts.estimate_params]
+        #             newvalues = mcmc.cur_params()[mcmc.estimate_idx]
+        #             oldvalues_array = numpy.array(oldvalues)
+        #             newvalues_array = numpy.array(newvalues)
+        #             change = numpy.log10(newvalues_array / oldvalues_array)
+        #             combined = numpy.column_stack((name, oldvalues, newvalues, change))
+        #             numpy.savetxt('calibration_H1666_highEGF_paramchange_findingnans7.txt', combined, delimiter=' ', fmt='%s')
+        #             numpy.save('calibration_alltestedparams_H1666_highEGF_findingnans7.npy', mcmc.positions)
+        #             numpy.save('calibration_allpositions_H1666_highEGF_findingnans7.npy', mcmc.get_mixed_accepts(burn=opts.nsteps/10))
+        #             numpy.save('calibration_fittedparams_H1666_highEGF_findingnans7.npy', zip(opts.estimate_params, mcmc.cur_params()[mcmc.estimate_idx]))
+        #             numpy.save('calibration_likelihoods_H1666_highEGF_findingnans7.npy', mcmc.likelihoods)
+        #             numpy.save('calibration_AKTlikelihoods_H1666_highEGF_findingnans7.npy', objAKT)
+        #             numpy.save('calibration_Erblikelihoods_H1666_highEGF_findingnans7.npy', objErb)
+        #             numpy.save('calibration_ERKlikelihoods_H1666_highEGF_findingnans7.npy', objERK)
+        #             sys.exit()
         return objAKT[mcmc.iter] + objErb[mcmc.iter] + objERK[mcmc.iter]
     except AttributeError:
          return  numpy.sum((yslice_exp_he - ysim_slice_he) ** 2 / (2 * yslice_exp_var_he ** 2))
@@ -89,15 +89,15 @@ def step(mcmc):
 
 # data is already scaled to 0-1
 splined_exp_pts = numpy.array([0,20,41,62,82,124,249,374,499,999])
-data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_H1666_highEGF_unnorm_splined.npy')
+data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_H1666_highEGF_unnorm_splined_nonzeroinit.npy')
 ydata_exp_he = numpy.load(data_filename)
 yslice_exp_he = ydata_exp_he[splined_exp_pts]
-var_data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_var_H1666_highEGF_unnorm_splined.npy')
+var_data_filename = os.path.join(os.path.dirname(__file__), 'experimental_data_var_H1666_highEGF_unnorm_splined_unweighted_nonzeroinit.npy')
 exp_var_he = numpy.load(var_data_filename) #Standard deviation was calculated from the mean by assuming a coefficient of variation of .25; sdev's equal to 0 were set to 1 to avoid division by 0 errors
 yslice_exp_var_he = exp_var_he[splined_exp_pts]
 
-tspan = numpy.linspace(0,7200, num=14400)
-exptimepts = numpy.array([0, 299, 599, 899, 1199, 1799, 3599, 5399, 7199, 14399])
+tspan = numpy.linspace(0,7200, num=144000)
+exptimepts = numpy.array([0, 2999, 5999, 8999, 11999, 17999, 35999, 53999, 71999, 143999])
 
 obs_names = ['obsAKTPP', 'obsErbB1_ErbB_P_CE', 'obsERKPP']
 
@@ -108,6 +108,7 @@ opts.integrator = 'vode'
 opts.nsteps = 50000
 opts.order = 5
 opts.method = 'bdf'
+# opts.ixpr = True
 opts.anneal_length = 12500
 opts.norm_step_size = .4
 
@@ -144,8 +145,8 @@ opts.prior_fn = prior
 opts.step_fn = step
 opts.seed = 1
 #opts.atol=numpy.load('calibration_atol_values.npy')
-opts.atol=1e-2
-opts.rtol=1e-2
+opts.atol=1e-3
+opts.rtol=1e-3
 opts.intsteps = 5000
 mcmc = bayessb.MCMC(opts)
 
@@ -162,7 +163,7 @@ for param, new_value in zip(opts.estimate_params, fitted_values):
     print '%-10s %-12.2g %-12.2g %-+6.2f' % values
     param_dict[param.name] = new_value
 
-with open('calibration_H1666_highEGF_fittedparams_unnorm_splined_fromhighEGFval_priorvar1.5_intchange', 'wb') as handle:
+with open('calibration_H1666_highEGF_fittedparams_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit', 'wb') as handle:
     pickle.dump(param_dict, handle)
 
 name = [p.name for p in opts.estimate_params]
@@ -172,11 +173,11 @@ oldvalues_array = numpy.array(oldvalues)
 newvalues_array = numpy.array(newvalues)
 change = numpy.log10(newvalues_array / oldvalues_array)
 combined = numpy.column_stack((name, oldvalues, newvalues, change))
-numpy.savetxt('calibration_H1666_highEGF_paramchange_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.txt', combined, delimiter=' ', fmt='%s')
-numpy.save('calibration_alltestedparams_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', mcmc.positions)
-numpy.save('calibration_allpositions_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', mcmc.get_mixed_accepts(burn=opts.nsteps/10))
-numpy.save('calibration_fittedparams_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', zip(opts.estimate_params, mcmc.cur_params()[mcmc.estimate_idx]))
-numpy.save('calibration_likelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', mcmc.likelihoods)
-numpy.save('calibration_AKTlikelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', objAKT)
-numpy.save('calibration_Erblikelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', objErb)
-numpy.save('calibration_ERKlikelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar1.5_intchange.npy', objERK)
+numpy.savetxt('calibration_H1666_highEGF_paramchange_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.txt', combined, delimiter=' ', fmt='%s')
+numpy.save('calibration_alltestedparams_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', mcmc.positions)
+numpy.save('calibration_allpositions_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', mcmc.get_mixed_accepts(burn=opts.nsteps/10))
+numpy.save('calibration_fittedparams_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', zip(opts.estimate_params, mcmc.cur_params()[mcmc.estimate_idx]))
+numpy.save('calibration_likelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', mcmc.likelihoods)
+numpy.save('calibration_AKTlikelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', objAKT)
+numpy.save('calibration_Erblikelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', objErb)
+numpy.save('calibration_ERKlikelihoods_H1666_highEGF_unnorm_splined_fromhighEGFval_priorvar4_somenans_nonzeroinit.npy', objERK)
